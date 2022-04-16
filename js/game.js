6acc7b3e71961bcf3609b8ca7f9046e0e4b0f8d8
zaida-hammel <zaida-hammel@users.noreply.github.com>
2022-04-16 20:36:44 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -8,8 +8,8 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
-}
+  backgroundColor: 0x5f6e7a,
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+console.log(game);
