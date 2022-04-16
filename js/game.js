ae02964d4027b0967a848dbf4ca9a00084088398
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-16 16:20:19 -0400

    deleted files and edited code

---
+++ js/game.js
@@ -0,0 +1,15 @@
+/* global Phaser */
+
+// Created by: Zaida Hammel
+// Created on: April 2022
+// This is the Phaser3 configuration file
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
-
-function myButtonClicked() {
-    // this function displays "Hello, World!"
-    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-  }
-  
