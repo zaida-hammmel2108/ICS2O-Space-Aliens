ae95ef3f8c467ec6984336abc504ee591b3dfd49
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-27 17:24:04 +0000

    added code

--- js/splashScene.js
+++ js/splashScene.js
@@ -12,6 +12,8 @@
 class SplashScene extends Phaser.Scene {
   constructor() {
     super({ key: "splashScene" });
+
+    this.splashSceneBackgroundImage = null
   }
 
   init(data) {
@@ -20,12 +22,23 @@ class SplashScene extends Phaser.Scene {
 
   preload() {
     console.log("Splash Scene");
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
 
-  create(data) {}
+  create(data) {
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0,
+      0,
+      "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
+  }
 
   update(time, delta) {
-    this.scene.switch("titleScene");
+    if (time > 3000) {
+      this.scene.switch("titleScene")
+    }
   }
 }
 export default SplashScene;
