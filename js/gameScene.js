5a284e9d1f604daf1e12fe039686f36612ee472f
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-10 17:34:29 +0000

    edited code

--- js/gameScene.js
+++ js/gameScene.js
@@ -33,7 +33,24 @@ class GameScene extends Phaser.Scene {
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    const keyLeftObj = this.input.keyboard.addKey ("LEFT")
+    const keyRightObj = this.input.keyboard.addKey ("RIGHT")
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
+  }
 }
 
 export default GameScene;
