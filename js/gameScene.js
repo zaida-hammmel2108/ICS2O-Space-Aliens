23176d6c5d8d35a71f9259c7659852c6fa177fc5
zaida-hammel <zaida-hammel@users.noreply.github.com>
2022-05-25 17:31:09 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,24 +7,27 @@
 // This is the Game Scene
 
 class GameScene extends Phaser.Scene {
-
-  createAlien () {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
-    anAlien.body.velocity.y = 200
-    anAlien.body.velocity.x = alienXVelocity
-    this.alienGroup.add(anAlien)
+  createAlien() {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1;
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1;
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1;
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien");
+    anAlien.body.velocity.y = 200;
+    anAlien.body.velocity.x = alienXVelocity;
+    this.alienGroup.add(anAlien);
   }
   constructor() {
     super({ key: "gameScene" });
 
-    this.ship = null
-    this.fireMissile = false
-    this.score = 0
-    this.scoreText = null
-    this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align: "center" }
+    this.ship = null;
+    this.fireMissile = false;
+    this.score = 0;
+    this.scoreText = null;
+    this.scoreTextStyle = {
+      font: "65px Arial",
+      fill: "#ffffff",
+      align: "center",
+    };
   }
 
   init(data) {
@@ -48,7 +51,12 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
     this.background.setOrigin(0, 0);
 
-    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(
+      10,
+      10,
+      "Score: " + this.score.toString(),
+      this.scoreTextStyle
+    );
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
@@ -61,15 +69,15 @@ class GameScene extends Phaser.Scene {
       this.missileGroup,
       this.alienGroup,
       function (missileCollide, alienCollide) {
-        alienCollide.destroy()
-        missileCollide.destroy()
-        this.sound.play("explosion")
-        this.score = this.score + 1
-        this.scoreText.setText("Score: " + this.score.toString())
-        this.createAlien()
-        this.createAlien()
-      }.bind(this))
-    
+        alienCollide.destroy();
+        missileCollide.destroy();
+        this.sound.play("explosion");
+        this.score = this.score + 1;
+        this.scoreText.setText("Score: " + this.score.toString());
+        this.createAlien();
+        this.createAlien();
+      }.bind(this)
+    );
   }
 
   update(time, delta) {
