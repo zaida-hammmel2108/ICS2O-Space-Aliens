0f6209eeaeb6315615c2d4761622a0778ddfc9bf
zaida-hammel <zaida-hammel@users.noreply.github.com>
2022-05-25 17:57:48 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -23,8 +23,16 @@ class GameScene extends Phaser.Scene {
     this.fireMissile = false;
     this.score = 0;
     this.scoreText = null;
-    this.scoreTextStyle = { font: "65px Arial", fill: "#ffffff", align: "center", }
-    this.gameOverTextStyle = { font: "65px Arial", fill: "#ff0000", align: "center" }
+    this.scoreTextStyle = {
+      font: "65px Arial",
+      fill: "#ffffff",
+      align: "center",
+    };
+    this.gameOverTextStyle = {
+      font: "65px Arial",
+      fill: "#ff0000",
+      align: "center",
+    };
   }
 
   init(data) {
@@ -35,21 +43,26 @@ class GameScene extends Phaser.Scene {
     console.log("Game Scene");
 
     //images
-    this.load.image("starBackground", "assets/starBackground.png")
-    this.load.image("ship", "assets/spaceShip.png")
-    this.load.image("missile", "assets/missile.png")
-    this.load.image("alien", "assets/alien.png")
+    this.load.image("starBackground", "assets/starBackground.png");
+    this.load.image("ship", "assets/spaceShip.png");
+    this.load.image("missile", "assets/missile.png");
+    this.load.image("alien", "assets/alien.png");
     //audio
-    this.load.audio("laser", "assets/laser1.wav")
-    this.load.audio("explosion", "assets/barrelExploding.wav")
-    this.load.audio("bomb", "assets/bomb.wav")
+    this.load.audio("laser", "assets/laser1.wav");
+    this.load.audio("explosion", "assets/barrelExploding.wav");
+    this.load.audio("bomb", "assets/bomb.wav");
   }
 
   create(data) {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0);
     this.background.setOrigin(0, 0);
 
-    this.scoreText = this.add.text(10, 10, "Score: " + this.score.toString(), this.scoreTextStyle)
+    this.scoreText = this.add.text(
+      10,
+      10,
+      "Score: " + this.score.toString(),
+      this.scoreTextStyle
+    );
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship");
 
@@ -62,24 +75,38 @@ class GameScene extends Phaser.Scene {
       this.missileGroup,
       this.alienGroup,
       function (missileCollide, alienCollide) {
-        alienCollide.destroy()
-        missileCollide.destroy()
-        this.sound.play("explosion")
-        this.score = this.score + 1
-        this.scoreText.setText("Score: " + this.score.toString())
-        this.createAlien()
-        this.createAlien()
-      }.bind(this))
+        alienCollide.destroy();
+        missileCollide.destroy();
+        this.sound.play("explosion");
+        this.score = this.score + 1;
+        this.scoreText.setText("Score: " + this.score.toString());
+        this.createAlien();
+        this.createAlien();
+      }.bind(this)
+    );
 
-    this.physics.add.collider(this.ship, this.alienGroup, function (shipCollide, alienCollide) {
-      this.sound.play("bomb")
-      this.physics.pause()
-      alienCollide.destroy()
-      shipCollide.destroy()
-      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, "Game Over!\nClick to play again.", this.gameOverTextStyle).setOrigin(0,5)
-      this.gameOverText.setInteractive({ useHandCursor: true })
-      this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
-    }.bind(this))
+    this.physics.add.collider(
+      this.ship,
+      this.alienGroup,
+      function (shipCollide, alienCollide) {
+        this.sound.play("bomb");
+        this.physics.pause();
+        alienCollide.destroy();
+        shipCollide.destroy();
+        this.gameOverText = this.add
+          .text(
+            1920 / 2,
+            1080 / 2,
+            "Game Over!\nClick to play again.",
+            this.gameOverTextStyle
+          )
+          .setOrigin(0, 5);
+        this.gameOverText.setInteractive({ useHandCursor: true });
+        this.gameOverText.on("pointerdown", () =>
+          this.scene.start("gameScene")
+        );
+      }.bind(this)
+    );
   }
 
   update(time, delta) {
